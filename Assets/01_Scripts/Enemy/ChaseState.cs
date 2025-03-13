6c056a59a987f1d9f18cb95897d9ab5691039231
ssolem <tjrals961@gmail.com>
2025-03-13 20:34:30 +0900

    feat : 몬스터 상태 패턴 구현
    * [feat] script 생성, 상태 패턴 기본
    
    * [feat] idle & prowl state
    
    * [feat] chase,attack,dead state 추가
    
    * [feat] damagable interface, take damage 추가

---
+++ Assets/01_Scripts/Enemy/ChaseState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : IEnemyState
+{
+    public void Enter(EnemyBase enemy)
+    {
+        enemy.animator.SetBool("Move", true);
+        enemy.SetChase();
+    }
+
+    public void Execute(EnemyBase enemy)
+    {
+        if (enemy.playerDistance <= enemy.attackRange)
+        {
+            enemy.ChangeState(EnemyStates.Attack);
+        }
+
+        if(enemy.playerDistance > enemy.detectDistance)
+        {
+            enemy.ChangeState(EnemyStates.Idle);
+        }
+    }
+
+    public void Exit(EnemyBase enemy)
+    {
+        enemy.animator.SetBool("Move", false);
+    }
+}
