e6b2554fcde04830879c38d3c72709fb36001821
subin <subin24890@gmail.com>
2025-03-14 12:15:04 +0900

    [fix] 조수빈_컨디션 ui 캐릭터 매니져 연결후 테스트 가능

--- Assets/JSB/ConditionsController.cs
+++ Assets/JSB/ConditionsController.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 using System.ComponentModel;
 using static UnityEngine.Rendering.DebugUI;
 
-public class ConditionsController : MonoBehaviour // MonoBehaviour
+public class ConditionsController : MonoBehaviour // IDamagable
 {
     //여기서 컨디션의 연산
     public ConditionHandler Health { get; private set; }
     public ConditionHandler Hunger { get; private set; }
     public ConditionHandler Thirst { get; private set; }
-    public ConditionHandler Stemina { get; private set; }
+    public ConditionHandler Stamina { get; private set; }
 
     private void Awake()
     {
         Health = new ConditionHandler(100f); 
         Hunger = new ConditionHandler(100f); 
-        Thirst = new ConditionHandler(100f); 
-        Stemina = new ConditionHandler(100f); 
+        Thirst = new ConditionHandler(100f);
+        Stamina = new ConditionHandler(100f); 
     }
 
     private void Start()
     {
-        InvokeRepeating("OnHungry", 5f, 5f);
-        InvokeRepeating("OnThirst", 5f, 5f);
+        InvokeRepeating("OnHungry", 5f, 5f);//배고픔 지속 감소 
+        InvokeRepeating("OnThirst", 5f, 5f);//목마름 지속 감소
     }
 
     private void Update()
@@ -31,18 +31,28 @@ public class ConditionsController : MonoBehaviour // MonoBehaviour
         
     }
 
-    
-  
-    public void Eat(float value)
+    /// <summary>
+    /// 음식 먹을때 실행
+    /// </summary>
+    /// <param name="value"></param>
+    public void Eat(float value)//아이템 데이터의값을 받아서 실행
     {
-        
+        Health.Value += value;
     }
 
+    /// <summary>
+    /// 음료 마실때 실행
+    /// </summary>
+    /// <param name="value"></param>
     public void Drink(float value)
     {
-
+        Thirst.Value += value;
     }
 
+    /// <summary>
+    /// 체력 포션 사용할때
+    /// </summary>
+    /// <param name="value"></param>
     public void Heal(float value)
     {
         if (Health.Value < Health.MaxValue)
@@ -51,6 +61,10 @@ public class ConditionsController : MonoBehaviour // MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 캐릭터가 죽을때
+    /// </summary>
+    /// <param name="value"></param>
     public void Die(float value)
     {
         if (Health.Value <= 0)
@@ -59,8 +73,10 @@ public class ConditionsController : MonoBehaviour // MonoBehaviour
             CancelInvoke("OnThirst");
         }
     }
-
-    #region 업데이트용 함수
+    /// <summary>
+    /// 컨디션 지속 감소
+    /// </summary>
+    #region 반복 실행 함수
     void OnHungry()
     {
         if (Hunger.Value > 0)
