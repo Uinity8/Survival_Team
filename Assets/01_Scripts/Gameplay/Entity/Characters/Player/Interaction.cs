9635d2fe7c218e2335f8c2d1d5437be1ed59ff91
yjsong2154 <59411999+yjsong2154@users.noreply.github.com>
2025-03-14 09:52:57 +0900

    10 interaction (#13)
    * Feat: Interaction 구현
    
    * Fix: 머지 이후 interaction 스크립트 조정

---
+++ Assets/01_Scripts/Gameplay/Entity/Characters/Player/Interaction.cs
@@ -0,0 +1,62 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.VisualScripting;
+
+public interface IInteractable
+{
+    public string GetPromptMessage();
+    public void OnInteract();
+}
+
+public class Interaction : MonoBehaviour
+{
+    public float checkRate = 0.05f;
+    private float lastCheckTime;
+    public float maxCheckDistance = 10f;
+    public LayerMask layerMask;
+
+    public GameObject curInteractGameObject;
+    private IInteractable curInteractable;
+
+    public TextMeshProUGUI promptText;
+    private Camera PlayerCamera;
+
+    void Start()
+    {
+        PlayerCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = PlayerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = curInteractable.GetPromptMessage();
+    }
+}
